6fdf6c7a618b29fcc6228fcf04074f1d7e35fb29
neojato <kpaul86+github@gmail.com>
2017-09-25 23:16:33 -0500

    update(server): update server config

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 import * as angularUniversal from 'angular-universal-express-firebase';
-export let ssrapp = angularUniversal.trigger({
-  index: __dirname + '/index.html',
+
+const app = angularUniversal.trigger({
+  index: __dirname + '/dist-server/index.html',
   main: __dirname + '/dist-server/main.bundle',
   enableProdMode: true,
   cdnCacheExpiry: 1200,
   browserCacheExpiry: 600
 });
+
+export let ssrapp = app;
