c209c4ec2fe197b90cc422adf88cd6bea4fa6347
neojato <kpaul86+github@gmail.com>
2017-09-28 15:19:32 -0500

    update(router): implement dynamic page titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
+import { SiteConfigService } from './admin/shared/site-config/site-config.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { SiteConfig } from './admin/shared/site-config/site-config';
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
 declare let ga: Function;
 
 @Component({
@@ -8,17 +17,49 @@ declare let ga: Function;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  siteConfig: FirebaseObjectObservable<SiteConfig>;
+  eventName: string;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private title: Title,
+    private siteConfigService: SiteConfigService
+  ) { }
 
   ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (!(event instanceof NavigationEnd)) {
-        return;
-      }
-      ga('set', 'page', event.urlAfterRedirects);
-      ga('send', 'pageview');
-      window.scrollTo(0, 0);
+    this.siteConfig = this.siteConfigService.getConfig();
+
+    this.siteConfig.subscribe(snap => {
+      this.eventName = snap.eventName;
     });
+
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map((route) => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => {
+        // dynamically set page titles
+        let pageTitle = this.title.getTitle();
+        if (this.eventName) {
+          pageTitle = this.eventName;
+        }
+        if (event['title']) {
+          pageTitle += ' :: ' + event['title'];
+        }
+        this.title.setTitle(pageTitle);
+        // Google Analytics track pageview
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+        // auto scroll to top on navigation
+        window.scrollTo(0, 0);
+      });
   }
 }
