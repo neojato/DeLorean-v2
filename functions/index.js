9867e286365f8ab92c41a809966af13f0e8bdd6d
Maye Edwin <mayedwine1@gmail.com>
2017-10-28 14:32:57 +0300

    Maye Edwin Config
    Maye Edwin Config

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,8 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
-
-exports.createProfile = functions.auth.user().onCreate(event => {
-  return admin.database().ref(`/userProfile/${event.data.uid}`).set({
-    displayName: event.data.displayName,
-    email: event.data.email,
-    photoURL: event.data.photoURL,
-    provider: 'Google'
-  });
-});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
