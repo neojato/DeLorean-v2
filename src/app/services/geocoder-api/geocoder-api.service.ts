456533738765545ffc72baafbf2aca8aec244100
neojato <kpaul86+github@gmail.com>
2018-07-31 22:08:04 -0500

    Upgrade Angular 4.0.0 > 6.1.0 & Angular CLI 1.6.8 > 6.1.1

--- src/app/services/geocoder-api/geocoder-api.service.ts
+++ src/app/services/geocoder-api/geocoder-api.service.ts
@@ -1,15 +1,15 @@
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { firebaseConfig } from './../../../environments/firebase.config';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class GeocoderApiService {
   apiKey: string;
   apiURL: string;
 
-  constructor(private http: Http) {
+  constructor(private http: HttpClient) {
     this.apiKey = firebaseConfig.mapsKey;
     this.apiURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${this.apiKey}&address=`;
   }
@@ -17,7 +17,7 @@ export class GeocoderApiService {
   findFromAddress(address: string): Observable<any> {
     const compositeAddress = encodeURIComponent(address.replace(/^\s+|\s+$/g, '+'));
     const url = `${this.apiURL}${compositeAddress}`;
-    return this.http.get(url).map(response => <any> response.json());
+    return this.http.get(url);
   }
 
 }
