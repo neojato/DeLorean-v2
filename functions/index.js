d8c354504e3ee87e2540df93d7f23a7ad0166f02
neojato <kpaul86+github@gmail.com>
2018-08-08 00:26:30 -0500

    update(functions): upgrade Firebase Functions to 2.x

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
-exports.createProfile = functions.auth.user().onCreate(event => {
-  return admin.database().ref(`/userProfile/${event.data.uid}`).set({
-    displayName: event.data.displayName,
-    email: event.data.email,
-    photoURL: event.data.photoURL,
+exports.createProfile = functions.auth.user().onCreate((userRecord, context) => {
+  return admin.database().ref(`/userProfile/${userRecord.uid}`).set({
+    displayName: userRecord.displayName,
+    email: userRecord.email,
+    photoURL: userRecord.photoURL,
     provider: 'Google'
   });
 });
